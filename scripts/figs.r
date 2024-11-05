aa63106ede991dad435c720066bf1c7bc6f43878
Gibran Hemani <explodecomputer@gmail.com>
2024-11-05 12:21:07 -0700

    upd

--- scripts/figs.r
+++ scripts/figs.r
@@ -11,24 +11,24 @@ l %>%
     group_by(gen, what, h2, rho) %>%
     summarise(r=median(r^2)) %>%
     mutate(gen=gen+1, what = case_when(what == "rare" ~ "Null rare variants", what == "common" ~ "Null common variants", what == "prs" ~ "Causal variants")) %>%
+    mutate(rholab = paste("rho ==", rho), h2lab = paste("h^2 ==", h2)) %>%
     ggplot(., aes(x=as.factor(gen), y=r, colour=as.factor(what))) + geom_line(aes(group=what)) +
     facet_grid(h2 ~ rho, labeller = label_both) +
     labs(x="Generation", y="Median R^2 between variant and PRS", colour="Variant") +
     scale_colour_brewer(type="qual")
-ggsave(here("results/run_sim_prs.png"), width=10, height=10)
+ggsave(here("results/run_sim_prs.pdf"), width=7, height=7)
 
 l %>% 
     filter(out == "y") %>%
     group_by(gen, what, h2, rho) %>%
     summarise(r=median(r^2)) %>%
     mutate(gen=gen+1, what = case_when(what == "rare" ~ "Null rare variants", what == "common" ~ "Null common variants", what == "prs" ~ "Causal variants")) %>%
+    mutate(rholab = paste("rho ==", rho), h2lab = paste("h^2 ==", h2)) %>%
     ggplot(., aes(x=as.factor(gen), y=r, colour=as.factor(what))) + geom_line(aes(group=what)) +
-    facet_grid(h2 ~ rho, labeller = label_both) +
-    labs(x="Generation", y="Median R^2 between variant and trait", colour="Variant") +
+    facet_grid(h2lab ~ rholab, labeller = label_parsed) +
+    labs(x="Generation", y=expression("Median" ~ R^2 ~ "between variant and trait"), colour="Variant") +
     scale_colour_brewer(type="qual")
-ggsave(here("results/run_sim_trait.png"), width=10, height=10)
+ggsave(here("results/run_sim_trait.pdf"), width=7, height=7)
 
-
-
-
+l$rholab <- paste0(".(rho)=", l$rho)
 
