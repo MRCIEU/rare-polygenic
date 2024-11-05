8f21a5cac29ad74606047ef7603561a395164558
Gibran Hemani <explodecomputer@gmail.com>
2024-11-05 12:20:58 -0700

    upd

--- scripts/basic_sim.r
+++ scripts/basic_sim.r
@@ -260,34 +260,3 @@ options <- furrr_options(seed=TRUE)
 l <- future_pmap(param, run_sim, .progress=TRUE, .options=options)
 l <- bind_rows(l)
 save(l, file=here("results/run_sim.RData"))
-
-
-
-load(file="run_sim.RData")
-
-l %>% 
-    filter(out == "prs") %>%
-    group_by(gen, what, h2, rho) %>%
-    summarise(r=median(r^2)) %>%
-    mutate(gen=gen+1, what = case_when(what == "rare" ~ "Null rare variants", what == "common" ~ "Null common variants", what == "prs" ~ "Causal variants")) %>%
-    ggplot(., aes(x=as.factor(gen), y=r, colour=as.factor(what))) + geom_line(aes(group=what)) +
-    facet_grid(h2 ~ rho, labeller = label_both) +
-    labs(x="Generation", y="Median R^2 between variant and PRS", colour="Variant") +
-    scale_colour_brewer(type="qual")
-ggsave(here("results/run_sim_prs.png"), width=10, height=10)
-
-l %>% 
-    filter(out == "y") %>%
-    group_by(gen, what, h2, rho) %>%
-    summarise(r=median(r^2)) %>%
-    mutate(gen=gen+1, what = case_when(what == "rare" ~ "Null rare variants", what == "common" ~ "Null common variants", what == "prs" ~ "Causal variants")) %>%
-    ggplot(., aes(x=as.factor(gen), y=r, colour=as.factor(what))) + geom_line(aes(group=what)) +
-    facet_grid(h2 ~ rho, labeller = label_both) +
-    labs(x="Generation", y="Median R^2 between variant and trait", colour="Variant") +
-    scale_colour_brewer(type="qual")
-ggsave(here("results/run_sim_trait.png"), width=10, height=10)
-
-
-
-
-
